e7d06d42c88ec632ea1752eb92bd1013f47fa65d
Jhon Michael <abbas.johnmichael.eccbsc@gmail.com>
2024-09-16 17:46:03 +0800

    update

--- constants/data.ts
+++
@@ -1,9 +0,0 @@
-import Babel from "@/public/banner1.png";
-
-export const Datas = [
-  {
-    title: "Babel Language Translator",
-    tech: ["Dart", "Flutter", "Firebase"],
-    image: Babel,
-  },
-];
