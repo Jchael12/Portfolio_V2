d3230788ba97b49ecedf5923df6c43c66c4fdf50
Jhon Michael <abbas.johnmichael.eccbsc@gmail.com>
2024-09-30 22:48:56 +0800

    update

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,7 +4,7 @@ import SparklesText from "./ui/sparkles-text";
 export default function Contact() {
   return (
     <div className="lg:w-6/12 sm:w-10-/12 flex flex-col items-center justify-center mb-10">
-      <p>( Need an elevated solution? )</p>
+      <p>Need an elevated solution?</p>
       <SparklesText
         text="
         Let's Make it Happen
@@ -12,7 +12,7 @@ export default function Contact() {
         className="my-10 text-3xl lg:text-4xl"
       />
       <a
-        href="mailto:jhonmichaelabbas0712@gmail.com?subject=Hire%20Me&body=Hi,%20I%20would%20like%20to%20discuss%20a%20project."
+        href="mailto:jhonmichaelabbas0712@gmail.com?subject=Let's%20Connect!&body=Hi,%20I%20would%20like%20to%20discuss%20a%20project."
         target="_blank"
         rel="noopener noreferrer"
       >
